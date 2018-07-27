b8fca0f679657fb0046094bdba266933877d39fc
toonraon <toonraon@naver.com>
2018-07-27 20:58:58 +0900

    폰트 선택 추가 작업 중

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// .input-auto-width의 가로 길이를 글자에 맞게 자동 조정
 $(".input-auto-width").on('keydown', function(e) {
 	var value = $(".input-auto-width").val();
 	$("body").append("<div id='virtual-input' style='display: inline-block;'>" + value + "</div>");
@@ -6,4 +7,9 @@ $(".input-auto-width").on('keydown', function(e) {
 
 	$(".input-auto-width").css("width", inputWidth);
 	$("#virtual-input").remove();
+});
+
+// 폰트 옵션에 해당 폰트 적용하기
+$("#font-select").each(function() {
+	$(this).css("font-family", $(this).val() + " sans-serif") ;
 });
